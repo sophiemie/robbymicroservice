eba0551372bcfbb33dbdd6bb09f3ca78f2446d04
Sophie Mießner <smiessner@stud.hs-bremen.de>
2024-10-29 13:07:59 +0100

    Robot Controller fertig schreiben

--- controller/robotController.js
+++ controller/robotController.js
@@ -23,7 +23,7 @@ const getStatus = (req, res) => {
     {
         res.status(404).send('Robot not found');
     }
-}
+};
 
 const move = (req, res) => {
     // Sucht einen Roboter anhand der ID aus URL Parameter mit Request
@@ -47,7 +47,7 @@ const move = (req, res) => {
     {
         res.status(404).send('Robot not found');
     }
-}
+};
 
 const pickUpItem = (req, res) => {
     const robot = robots[req.params.id];
@@ -63,7 +63,7 @@ const pickUpItem = (req, res) => {
     {
         res.status(404).send('Robot not found');
     }
-}
+};
 
 const putDownItem = (req, res) => {
     const robot = robots[req.params.id];
@@ -77,7 +77,7 @@ const putDownItem = (req, res) => {
         if (index > -1)
         {
             robot.inventory.splice(index, 1); // Item aus der Liste entfernen
-            robot.actions.push('Put down item $(itemID)')
+            robot.actions.push('Put down item $(itemID)');
             res.json(robot);
         }
     }
@@ -85,43 +85,77 @@ const putDownItem = (req, res) => {
     {
         res.status(404).send('Robot not found');
     }
-}
+};
 
 const updateStatus = (req, res) => {
     const robot = robots[req.params.id];
 
     if (robot)
     {
+        // Ueberpruefen ob ein Energy-Wert vorhanden ist
+        // Wird auf undefined ueberprueft, da 0 auch ein valider Wert sein kann
+        if (req.body.energy !== undefined) 
+        {
+            robot.energy = req.body.energy; // Energielevel auf neuen Wert setzen
+            robot.actions.push('Updated energy to ${robot.energy}');
+        }
 
+        // Ueberprueft ob eine Position zurueckgegeben wurde
+        // Hier wird nur einmal geprueft ob ein Position Objekt vorhanden ist
+        if (req.body.position)
+        {
+            robot.position = req.body.position; // Aktualisiert die Position
+            robot.actions.push('Updated position to ${JSON.stringify(robot.position)}');
+        }
+
+        res.json(robot); // Neue Werte in JSON schreiben
     }
     else
     {
         res.status(404).send('Robot not found');
     }
-}
+};
 
 const getActions = (req, res) => {
     const robot = robots[req.params.id];
 
     if (robot)
     {
-
+        res.json(robot.actions); // Aktionen schicken
     }
     else
     {
         res.status(404).send('Robot not found');
     }
-}
+};
 
 const attackRobot = (req, res) => {
     const robot = robots[req.params.id];
+    const target = robots[req.params.targetID];
 
-    if (robot)
+    if (robot && target)
     {
-
+        const damage = 10;
+        attacker.energy -= 5;
+        target.energy -= damage;
+        attacker.actions.push('Attacked robot ${target.id}');
+        res.json({ robot, target });
     }
     else
     {
-        res.status(404).send('Robot not found');
+        res.status(404).send('Robot or Target not found');
     }
-}
+};
+
+
+// Funktionen als public definieren
+module.exports = 
+{
+    getStatus,
+    move,
+    pickUpItem,
+    putDownItem,
+    updateStatus,
+    getActions,
+    attackRobot
+};
